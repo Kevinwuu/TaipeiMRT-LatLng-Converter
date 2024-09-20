2a84c3bbc5c2deef18e78bb47e870388ce9a5c77
Ethan Wu <405417048@gms.tku.edu.tw>
2024-09-21 03:22:51 +0800

    Update all file

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# latlngTransform
-Using Google geocoding API transform Taipei MRT latitude &amp; lontitude into address.
+# TaipeiMRT-LatLng-Converter
+
+## Intro
+
+Request from friend.
+
+## Feature
+
+Using the Google Geocoding API to convert Taipei MRT latitude and longitude data into addresses.
+
+[Google Geocoding API](https://developers.google.com/maps/documentation/geocoding/overview?hl=zh-tw)
+
+Source data is from `data.xlsx`.
+
+## Getting Started
+
+1. Install dependency
+
+    ```shell
+      npm install
+    ```
+
+2. Get Google API key
+
+   => Login into [Google Cloud Platform](https://console.cloud.google.com/)
+
+   => Create Project
+
+   => Activate Geocoding API service
+
+   => Create Certificate(API key)
+
+3. set env file
+
+    - change `.env.example` => `.env`
+
+    - set `API_KEY`
+
+## Usage
+
+```shell
+npm install
+
+node ./index.js
+```
+
+## Screenshot
+
+![alt text](/screenshot/image.png)
--- index.js
+++ index.js
@@ -1,34 +1,58 @@
-const axios = require('axios');
-var xlsx = require('node-xlsx');
+const axios = require("axios");
+const xlsx = require("node-xlsx");
+require("dotenv").config();
 
-const workSheetsFromFile = xlsx.parse('data.xlsx');
-form_data = workSheetsFromFile[0].data;
+const GOOGLE_GEOCODING_API_URL =
+  "https://maps.googleapis.com/maps/api/geocode/json";
+const API_KEY = process.env.API_KEY;
 
-var location = [];
-getData();
+let location = [];
 
-async function getData() {
-    for (var i = 1; i < form_data.length; i++) {
-        await getAddress(form_data[i][2], form_data[i][3]);
-    }
-    await console.log(location);
+/**
+ * 從sheet上面第一行往下讀資料, 把經緯度資料塞進API
+ *
+ * @example
+ * [ 'no', 'station', 'latitude', 'longitude' ],
+ * [ 1, '忠孝復興站', 25.041629, 121.543767 ],
+ * [ 2, '南港展覽館站', 25.0553846, 121.6182655 ],
+ */
+async function parseSheetData() {
+  console.log("processing...");
+  const workSheetsFromFile = xlsx.parse("data.xlsx");
+  form_data = workSheetsFromFile[0].data;
+  for (var i = 1; i < form_data.length; i++) {
+    await latLngToAddress(form_data[i][2], form_data[i][3]);
+  }
+  console.log(location);
 }
 
-async function getAddress(latitude, longitude) {
-    await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-            params: {
-                latlng: latitude + ',' + longitude,
-                language: 'zh-TW',
-                key: 'AIzaSyDZAQbZ2YE23m1_u6YCZAN63GHtkT6W608'
-            }
-        })
-        .then(function(response) {
-            // return response;
-            // console.log(response.data.results[0].formatted_address);
-            location.push(response.data.results[0].formatted_address)
-        })
-        .catch(function(error) {
-            // Error
-            console.log(error);
-        })
+/**
+ * convert latitude & longitude to address
+ * @param {number} latitude
+ * @param {number} longitude
+ * @returns
+ */
+async function latLngToAddress(latitude, longitude) {
+  await axios
+    .get(GOOGLE_GEOCODING_API_URL, {
+      params: {
+        latlng: latitude + "," + longitude,
+        language: "zh-TW",
+        key: API_KEY,
+      },
+    })
+    .then(function (response) {
+      const hasError = !!response.data.error_message;
+      if (hasError) {
+        console.log("[response error]:", response.data);
+      } else {
+        const addressResult = response.data.results[0].formatted_address;
+        location.push(addressResult);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
+
+parseSheetData();
